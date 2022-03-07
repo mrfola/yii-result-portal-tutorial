400b1f238ea6a58470f93b4815d72ee583797fdd
Folaranmi Jesutofunmi <folaranmijesutofunmi@gmail.com>
2022-03-07 14:37:01 +0100

    Updated migration

---
+++ migrations/m220307_133337_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%users}}`.
+ */
+class m220307_133337_create_users_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%users}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(64),
+            'email' => $this->string(64),
+            'password' => $this->string(120),
+            'user_type' => $this->string(120)->defaultValue('user'),
+            'auth_key' =>  $this->string(100),
+            'created_at' => $this->dateTime()->defaultValue(Date('Y-m-d H:i:s')),
+            'updated_at' => $this->dateTime()->defaultValue(Date('Y-m-d H:i:s')),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%users}}');
+    }
+}
+
+
