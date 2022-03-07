6c282d6ef863495158c8369b83d52f2d4e8a241d
Folaranmi Jesutofunmi <folaranmijesutofunmi@gmail.com>
2022-03-07 14:41:42 +0100

    Updated migration

--- migrations/m220223_155050_create_users_table.php
+++ migrations/m220223_155050_create_users_table.php
@@ -17,6 +17,7 @@ class m220223_155050_create_users_table extends Migration
             'name' => $this->string(64),
             'email' => $this->string(64),
             'password' => $this->string(120),
+            'user_type' => $this->string(120)->defaultValue('user'),
             'auth_key' =>  $this->string(100),
             'created_at' => $this->dateTime()->defaultValue(Date('Y-m-d H:i:s')),
             'updated_at' => $this->dateTime()->defaultValue(Date('Y-m-d H:i:s')),
